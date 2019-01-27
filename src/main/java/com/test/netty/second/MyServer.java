373afc436654c115e48ab9ee642a9f9f3f654a46
elm <yinke.zhang@elm.me>
2019-01-28 07:50:46 +0800

    commit

--- src/main/java/com/test/netty/second/MyServer.java
+++ src/main/java/com/test/netty/second/MyServer.java
@@ -5,6 +5,8 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 public class MyServer {
 
@@ -16,15 +18,43 @@ public class MyServer {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception{
+
+        /**
+         * 1.为何创建两个group
+         * 2.接受用户端请求 bossGroup
+         * 3.处理用户端请求 workerGroup
+         *
+         * EventLoopGroup 事件循环组 死循环
+         * NioEventLoopGroup  异步的事件循环组 底层死循环
+         *
+         * EventLoopGroup bossGroup = new NioEventLoopGroup(1); 1代表一个线程；核心数*2
+         *
+         * 作用：完成准备工作 属性的赋值操作
+         * ThreadFactory 创建线程
+         */
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
+            /**
+             * 辅助类
+             */
             ServerBootstrap serverBootstrap = new ServerBootstrap();
+            /**
+             * 方法链编程
+             * group 赋值操作 注意类转化
+             * channel 方法接受.class 大部分与反射有关
+             *
+             *
+             */
             serverBootstrap.group(bossGroup,workerGroup)
                     .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.WARN))
                     .childHandler(new MyServerInitializer());
+            //服务器的启动 逻辑梳理 关键
             ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+
+            //
             channelFuture.channel().closeFuture().sync();
         }finally {
             bossGroup.shutdownGracefully();
---
+++ src/main/java/com/test/source/SourceTest.java
@@ -0,0 +1,14 @@
+package com.test.source;
+
+import io.netty.util.NettyRuntime;
+import io.netty.util.internal.SystemPropertyUtil;
+
+public class SourceTest {
+
+    public static void main(String[] args) {
+
+       int result = Math.max(1, SystemPropertyUtil.getInt(
+                "io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
+        System.out.println(result);
+    }
+}
