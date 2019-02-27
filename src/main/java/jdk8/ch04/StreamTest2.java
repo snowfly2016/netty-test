04757b72cc5f781ad0fcdd8bfa526c126951859d
elm <yinke.zhang@elm.me>
2019-02-27 20:38:04 +0800

    commit

--- src/main/java/jdk8/ch04/StreamTest2.java
+++ src/main/java/jdk8/ch04/StreamTest2.java
@@ -17,5 +17,16 @@ public class StreamTest2 {
        // list.stream().parallel().forEach(System.out::println);
         //有中间操作，调用ReferencePipeline的foreach方法，
        // list.stream().map(i->i).forEach(System.out::println);
+
+
+        //
+        List<String> list1 = Arrays.asList("hello","world","welcome","a","b","c");
+        Stream<String> stream = list1.stream();
+
+        System.out.println("-------------------------------");
+
+        Stream<String> stream1 = stream.map(i->i);
+
+
     }
 }
